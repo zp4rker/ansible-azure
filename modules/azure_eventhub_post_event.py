4580b8764de5ce1db7d1aedb032f1ea3cfaa8fc0
zp4rker <iam@zp4rker.com>
2021-09-06 15:56:39 +1000

    starting to implement AzureRMModuleBase

---
+++ modules/azure_eventhub_post_event.py
@@ -0,0 +1,32 @@
+from ansible.module_utils.azure_rm_common import AzureRMModuleBase
+
+class AzureRMEventHubPostEvent(AzureRMModuleBase):
+
+	def __init__(self):
+
+		self.module_arg_spec = dict(
+			connection_string=dict(type='str', required=True),
+			event_format=dict(type='str', default='json'),
+			event_data=dict(type='dict', required=True)
+        )
+
+        self.results = dict(
+            changed=False
+        )
+
+        required_if = []
+
+        self.connection_string = None
+        self.event_format = None
+        self.event_data = None
+
+        super(AzureRMAutoScale, self).__init__(self.module_arg_spec, supports_check_mode=True, required_if=required_if)
+
+
+    def exec_module(self, **kwargs):
+
+        for key in list(self.module_arg_spec.keys()) + ['tags']:
+            setattr(self, key, kwargs[key])
+
+        results = None
+        changed = False
